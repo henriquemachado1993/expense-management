3cdccced0b1d9a15071f88d2f336c0dd131416e9
Henrique machado <henriquemachado1993@gmail.com>
2023-05-09 19:29:41 -0300

    Adição de gráficos para despesas por mes e categorias.

---
+++ ExpenseManagement/Shared/ValueObjects/ChatDataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.Shared.ValueObjects
+{
+    public class DataItem<T>
+    {
+        public string Key { get; set; }
+        public T Value { get; set; }
+    }
+}
